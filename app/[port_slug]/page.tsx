9bbb5403a577d6a1252e314bde6f1cc57956704c
Kenneth Bass <kenny_bass@outlook.com>
2023-12-09 14:42:55 -0600

    move portfolio page path

--- app/[port_slug]/page.tsx
+++ app/[port_slug]/page.tsx
@@ -1,22 +1,46 @@
+import { InformationForm } from "@/components/form/information";
+import { Information, Social } from "@/interfaces/information";
+import { Portfolios } from "@/interfaces/portfolios";
 import { PORTFOLIO_COLLECTION_ID, database_service } from "@/lib/appwrite";
 import { Query } from "appwrite";
 
-async function fetchPortfolio(slug: string) {
-  const response = await database_service.list<any>(PORTFOLIO_COLLECTION_ID, [
-    Query.equal("slug", slug),
-    Query.limit(1),
-  ]);
+async function fetchInformation(port_slug: string) {
+  const response = await database_service.list<Portfolios>(
+    PORTFOLIO_COLLECTION_ID,
+    [Query.equal("slug", port_slug), Query.limit(1)],
+  );
 
-  return response.documents[0];
+  const portfolio = response.documents[0];
+  const { information } = portfolio;
+
+  if (information.length > 0) {
+    const social = information[0].social.map((item) => {
+      return JSON.parse(item as string) as Social;
+    });
+
+    const info: Information = {
+      ...information[0],
+      social,
+    };
+
+    return info;
+  }
+
+  return information[0];
 }
 
-export default async function Page({
+export default async function Portfolio({
   params,
 }: {
   params: { port_slug: string };
 }) {
   const { port_slug } = params;
-  const portfolio = await fetchPortfolio(port_slug);
+  const information = await fetchInformation(port_slug);
 
-  return <div>{portfolio.title}</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="text-2xl font-bold">Portfolio</h3>
+      <InformationForm data={information} />
+    </div>
+  );
 }
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -13,6 +13,12 @@ import {
   LucidePlus,
 } from "lucide-react";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
 
 export const Sidebar = () => {
   const { current } = usePortfolioStore();
@@ -28,11 +34,20 @@ export const Sidebar = () => {
       <ul className="flex w-full flex-1 flex-col gap-2 overflow-y-auto p-2">
         <li className="flex w-full flex-row gap-2">
           <PortfoliosSelect />
-          <Button asChild size="icon" variant="ghost">
-            <Link href={`/portfolio/create`} className="flex-none">
-              <LucidePlus className="h-4 w-4" />
-            </Link>
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button asChild size="icon" variant="ghost">
+                  <Link href={`/portfolio/create`} className="flex-none">
+                    <LucidePlus className="h-4 w-4" />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side="right">
+                <p className="capitalize">Create a portfolio.</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </li>
         {current ? (
           <>
@@ -42,7 +57,7 @@ export const Sidebar = () => {
                 variant="ghost"
                 className="flex flex-1 flex-row justify-start gap-4"
               >
-                <Link href={`/${current.slug}/portfolio`}>
+                <Link href={`/${current.slug}`}>
                   <LucideFolderOpen className="h-4 w-4" />
                   Portfolio
                 </Link>
@@ -59,14 +74,23 @@ export const Sidebar = () => {
                   Projects
                 </Link>
               </Button>
-              <Button asChild size="icon" variant="ghost">
-                <Link
-                  href={`/${current.slug}/projects/create`}
-                  className="flex-none"
-                >
-                  <LucidePlus className="h-4 w-4" />
-                </Link>
-              </Button>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button asChild size="icon" variant="ghost">
+                      <Link
+                        href={`/${current.slug}/projects/create`}
+                        className="flex-none"
+                      >
+                        <LucidePlus className="h-4 w-4" />
+                      </Link>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent side="right">
+                    <p className="capitalize">Create a project.</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </li>
             {/* <li className="flex w-full flex-row gap-2">
               <Button

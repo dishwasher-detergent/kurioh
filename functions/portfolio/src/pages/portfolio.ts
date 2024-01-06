ba3d58575aa0160f1550cec7b86a83ef0dbd499d
Kenneth Bass <kenny_bass@outlook.com>
2024-01-06 17:30:18 -0600

    add icon image

--- functions/portfolio/src/pages/portfolio.ts
+++ functions/portfolio/src/pages/portfolio.ts
@@ -1,5 +1,10 @@
 import { Hono } from 'hono';
-import { PORTFOLIO_COLLECTION_ID, database_service } from '../lib/appwrite.js';
+import {
+  PORTFOLIO_BUCKET_ID,
+  PORTFOLIO_COLLECTION_ID,
+  database_service,
+  storage_service,
+} from '../lib/appwrite.js';
 import { Portfolios } from '../types/types.js';
 
 export function Portfolios(app: Hono, cacheDuration: number = 1440) {
@@ -42,5 +47,41 @@ export function Portfolios(app: Hono, cacheDuration: number = 1440) {
     return c.json(prunedResponse, 200, {
       'Cache-Control': `public, max-age=${cacheDuration}`,
     });
+  });
+
+  app.get('/portfolios/:portfolio_slug/image', async (c) => {
+    const portfolio_slug = c.req.param('portfolio_slug');
+    const queryParams = c.req.query();
+
+    const response = await database_service.get<Portfolios>(
+      PORTFOLIO_COLLECTION_ID,
+      portfolio_slug
+    );
+
+    if (!response) {
+      return c.json({ error: 'Portfolio not found' }, 404);
+    }
+
+    const information = response.information[0];
+
+    if (!information) {
+      return c.json({ error: 'Information not found' }, 404);
+    }
+
+    if (!information.icon) {
+      return c.json({ error: 'Image not found' }, 404);
+    }
+
+    const file = await storage_service.getFilePreview(
+      PORTFOLIO_BUCKET_ID,
+      information.icon,
+      {
+        ...queryParams,
+      }
+    );
+
+    c.header('Content-Type', `image/png`);
+    c.header('Cache-Control', `public, max-age=${cacheDuration}`);
+    return c.body(file);
   });
 }

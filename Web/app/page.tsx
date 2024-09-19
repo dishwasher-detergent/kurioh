4762a0cf7ca0bea63c919026a8439cee4ba95543
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2024-09-19 09:37:50 -0500

    make it wait until the data has been refreshed

--- Web/app/page.tsx
+++ Web/app/page.tsx
@@ -2,17 +2,20 @@
 
 import { usePortfolioStore } from "@/store/zustand";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Main() {
+  const [times, setTimes] = useState(0);
   const { current } = usePortfolioStore();
   const router = useRouter();
 
   useEffect(() => {
+    if(times == 0 && !current) return;
+
     if (current) {
       router.push(current.id);
     } else {
       router.push("/portfolio/create");
     }
-  }, []);
+  }, [current]);
 }

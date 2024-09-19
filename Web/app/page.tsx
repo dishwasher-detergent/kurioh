78d6525d63233f84ebb735fb7e3f69026912e7de
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2024-09-19 10:10:18 -0500

    back to init

--- Web/app/page.tsx
+++ Web/app/page.tsx
@@ -2,19 +2,13 @@
 
 import { usePortfolioStore } from "@/store/zustand";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function Main() {
   const { current, update } = usePortfolioStore();
   const router = useRouter();
-  const [init, setInit] = useState(true);
 
   useEffect(() => {
-    if(!current && init) {
-      setInit(false);
-      return;
-    }
-
     if (current) {
       router.push(current.id);
     } else {

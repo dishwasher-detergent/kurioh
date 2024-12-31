2eb27ca20d175c9adc6435b827a2ad13076c97b6
Kenneth Bass <kenny_bass@outlook.com>
2024-12-30 19:50:24 -0600

    fix mobiel

--- Web/components/ui/header.tsx
+++ Web/components/ui/header.tsx
@@ -1,3 +1,4 @@
+import { LucideExternalLink } from "lucide-react";
 import Link from "next/link";
 import { Skeleton } from "./skeleton";
 
@@ -17,23 +18,24 @@ export function Header({
   children,
 }: HeaderProps) {
   return (
-    <header className="mb-8 flex w-full items-center justify-between">
+    <header className="mb-8 flex w-full items-start justify-between gap-4">
       <div className="space-y-2">
         {!loading ? (
-          <p className="text-sm text-muted-foreground">{slug}</p>
+          <p className="truncate text-sm text-muted-foreground">{slug}</p>
         ) : (
           <Skeleton className="h-6 w-20" />
         )}
         {!loading ? (
-          <h1 className="text-3xl font-bold">{title}</h1>
+          <h1 className="truncate text-3xl font-bold">{title}</h1>
         ) : (
           <Skeleton className="h-8 w-36 max-w-full" />
         )}
         {!loading && endpoint ? (
           <p className="text-sm">
             Endpoint:&nbsp;
-            <Link href={endpoint} target="_blank">
+            <Link href={endpoint} target="_blank" className="break-all">
               {endpoint}
+              <LucideExternalLink className="ml-2 inline size-3.5" />
             </Link>
           </p>
         ) : (

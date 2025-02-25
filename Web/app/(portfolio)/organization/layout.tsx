eddff430230adbb61c7c5667b9708d0dabba0e76
Kenneth Bass <kenny_bass@outlook.com>
2025-02-24 20:12:29 -0600

    update styles

--- Web/app/(portfolio)/organization/layout.tsx
+++ Web/app/(portfolio)/organization/layout.tsx
@@ -4,8 +4,10 @@ export default async function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="mx-auto min-h-full max-w-6xl p-4 px-4 md:px-8">
-      {children}
+    <main className="flex w-full grow flex-col bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] [--pattern-fg:var(--color-muted)]/50 dark:[--pattern-fg:var(--color-muted)]/10">
+      <div className="border-border bg-background mx-auto w-full max-w-6xl flex-1 border-x border-dashed p-4 px-4 md:px-8">
+        {children}
+      </div>
     </main>
   );
 }
